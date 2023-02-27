7fd6bdb66d234f26c099bbd4806607ead880b608
Ian Neo <ian.neo@checkbox.ai>
2023-02-28 00:02:54 +0800

    Added packages

---
+++ src/components/Button/button.stories.tsx
@@ -0,0 +1,9 @@
+import { Button } from "./button";
+
+// eslint-disable-next-line import/no-anonymous-default-export
+export default {
+    title: "Button",
+    component: Button,
+};
+
+export const Default = () => <Button>Click me!</Button>;
---
+++ src/components/Button/button.tsx
@@ -0,0 +1,12 @@
+import { ReactNode } from "react";
+
+interface ButtonProps {
+    children: ReactNode;
+}
+
+const Button = ({ children, ...props }: ButtonProps) => (
+    <button {...props}>{children}</button>
+);
+
+export { Button };
+export type { ButtonProps };
