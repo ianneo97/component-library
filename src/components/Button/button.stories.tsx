55782589172e6c03def62d55d1e79bc6106ab23e
Ian Neo <ian.neo@checkbox.ai>
2023-02-28 22:50:02 +0800

    Remove base button component

--- src/components/Button/button.stories.tsx
+++
@@ -1,9 +0,0 @@
-import { Button } from "./button";
-
-// eslint-disable-next-line import/no-anonymous-default-export
-export default {
-    title: "Button",
-    component: Button,
-};
-
-export const Default = () => <Button>Click me!</Button>;
